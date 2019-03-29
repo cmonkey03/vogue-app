33733162b8938dfabfd83c1c4985c9928e1af292
Russell Brown <info@russellpatrickbrown.com>
2019-03-29 14:13:27 -0400

    Added activeItem state to desktop nav

--- src/components/DesktopNavigation.js
+++ src/components/DesktopNavigation.js
@@ -26,10 +26,12 @@ class DesktopNavigation extends Component {
 
   hideFixedMenu = () => this.setState({ fixed: false })
   showFixedMenu = () => this.setState({ fixed: true })
+  handleClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
     const { children } = this.props
     const { fixed } = this.state
+    const { activeItem } = this.state
 
     return (
       <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
@@ -56,12 +58,39 @@ class DesktopNavigation extends Component {
                   VogueApp
                 </Menu.Item>
                 <Menu.Menu position='right'>
-                  <Menu.Item as='a' active>Home</Menu.Item>
-                  <Menu.Item as='a'>Why us</Menu.Item>
-                  <Menu.Item as='a'>Contact us</Menu.Item>
-                  <Menu.Item as='a'>Login</Menu.Item>
+                  <Menu.Item
+                    as='a'
+                    name='home'
+                    active={activeItem==='home'}
+                    onClick={this.handleClick}
+                    >Home</Menu.Item>
+                  <Menu.Item
+                    as='a'
+                    name='why us'
+                    active={activeItem==='why us'}
+                    onClick={this.handleClick}
+                    >Why us</Menu.Item>
+                  <Menu.Item
+                    as='a'
+                    name='contact us'
+                    active={activeItem==='contact us'}
+                    onClick={this.handleClick}
+                    >Contact us</Menu.Item>
+                  <Menu.Item
+                    as='a'
+                    name='login'
+                    active={activeItem==='login'}
+                    onClick={this.handleClick}
+                    >Login</Menu.Item>
                   <Menu.Item>
-                    <Button as='a' primary={fixed} style={{ marginLeft: '0.5em' }}>
+                    <Button
+                      as='a'
+                      primary={fixed}
+                      style={{ marginLeft: '0.5em' }}
+                      name='support'
+                      active={activeItem==='support'}
+                      onClick={this.handleClick}
+                      >
                       Support
                     </Button>
                   </Menu.Item>

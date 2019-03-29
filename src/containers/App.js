eb5aeebaddbe45acfa98ce1790ebece84df486d4
Russell Brown <info@russellpatrickbrown.com>
2019-03-28 21:08:37 -0400

    Added Semantic-UI-React homepage layout demo code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import '../App.css';
+import HomepageLayout from './HomepageLayout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <HomepageLayout />
       </div>
     );
   }

5bb319a387ada2ef0c9c0ad96a01e52231a2f2a7
Russell Brown <info@russellpatrickbrown.com>
2019-04-01 23:59:30 -0400

    Added more icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,7 +39,8 @@ const Footer = () => {
               <p>
                 Extra space for a call to action inside the footer that could help re-engage users.
               </p>
-              <Icon name='facebook'/>
+              <Icon  />
+              <Icon />
             </Grid.Column>
           </Grid.Row>
         </Grid>

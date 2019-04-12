6cf300babacc31ce6ac471bd39e024efdaa5c120
Russell Brown <info@russellpatrickbrown.com>
2019-04-12 17:06:52 -0400

    Changed h2 text

--- src/components/HomepageHeading.js
+++ src/components/HomepageHeading.js
@@ -26,7 +26,7 @@ const HomepageHeading = ({ mobile }) => (
     />
     <Header
       as='h2'
-      content='Do whatever you want when you want to.'
+      content='Learn death drops in no time!'
       inverted
       style={{
         fontSize: mobile ? '1.5em' : '1.7em',
